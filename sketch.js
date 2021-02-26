b136a9580acb674037075fabcff85ce2d2caf9bc
shivajahnavi <seshidhar111@gmail.com>
2021-02-26 19:36:57 +0530

    add plat form

--- sketch.js
+++ sketch.js
@@ -2,9 +2,11 @@ const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 
+
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var platform;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,6 +19,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform = new Ground (180,305,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -58,5 +61,6 @@ function draw(){
     log4.display();
     log5.display();
 
+    platform.display();
     bird.display();
 }
